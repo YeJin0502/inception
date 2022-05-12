712fc29f77223cfa30361965867c609e0dce9bbb
Yejin Song <yejsong@c3r5s5.42seoul.kr>
2022-05-12 19:17:50 +0900

    FIX hosts.sh

--- srcs/requirements/nginx/conf/hosts.sh
+++ srcs/requirements/nginx/conf/hosts.sh
@@ -2,7 +2,7 @@
 
 grep -E "yejsong.42.fr" /etc/hosts > /dev/null 2>&1
 
-if [ $? -eq 0 ]; then
+if [ $? -ne 0 ]; then
 	echo "127.0.0.1	www.yejsong.42.fr" >> /etc/hosts
 	echo "127.0.0.1 yejsong.42.fr" >> /etc/hosts
 fi
